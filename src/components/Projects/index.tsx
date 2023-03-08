8c1a7990e6da2172d6dfe884d3ae95e1c56a4ae8
Maurício Bruno <mb.bruno.s@gmail.com>
2023-03-08 09:33:42 -0300

    feat: adds repowars project

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import devconnect from "../../assets/devconnect.webp";
+import repowars from "../../assets/repowars.webp";
 import { ProjectCard } from "./ProjectCard";
 
 export const Projects: React.FC = () => {
@@ -13,6 +14,16 @@ export const Projects: React.FC = () => {
         code: "https://github.com/MBrunoS/devconnect",
       },
       badges: ["Next.js", "Typescript", "Prisma", "Markdoc"],
+    },
+    {
+      title: "RepoWars",
+      description: "Jogo de batalha de repositórios do GitHub",
+      imgSrc: repowars,
+      links: {
+        demo: "https://repowars.mbrunos.dev/",
+        code: "https://github.com/MBrunoS/repowars",
+      },
+      badges: ["React", "Typescript", "Vite", "React Icons"],
     },
   ];
 
