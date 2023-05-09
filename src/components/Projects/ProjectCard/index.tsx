cb3b41d9425f3df87f0cf10c9bc28ce7eaacf511
Maurício Bruno <mb.bruno.s@gmail.com>
2023-05-09 18:47:48 -0300

    feat: update projects section

--- src/components/Projects/ProjectCard/index.tsx
+++ src/components/Projects/ProjectCard/index.tsx
@@ -2,56 +2,68 @@ import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "../../Button";
-import { Card } from "../../Card";
 
 interface ProjectCardProps {
   title: string;
   imgSrc: StaticImageData;
-  children: React.ReactNode;
   links: { demo?: string; code?: string };
   badges: string[];
+  description: string;
 }
 
 export const ProjectCard: React.FC<ProjectCardProps> = ({
   title,
   imgSrc,
   links,
-  children,
   badges,
+  description,
 }) => {
   return (
-    <div className="flex flex-col items-center md:w-1/2 lg:w-1/3">
-      <Image
-        src={imgSrc}
-        alt={title}
-        placeholder="blur"
-        className="w-4/5 rounded-2xl border border-solid border-highlight-color -mb-8 z-10"
-      />
+    <div className="flex flex-col gap-6 w-full lg:w-5/6">
+      <div
+        className="px-3 py-6 md:py-3 bg-card-gradient border border-solid border-highlight-color
+                rounded-2xl border-1 text-secondary-color flex-grow md:text-left
+                flex flex-col items-center gap-x-8 md:flex-row"
+      >
+        <Image
+          src={imgSrc}
+          alt={title}
+          placeholder="blur"
+          className="w-4/5 max-w-xs md:w-2/5 rounded-2xl border border-solid border-highlight-color"
+        />
 
-      <Card>
-        <h3 className="font-bold text-2xl mt-5 mb-2 text-white">{title}</h3>
-        {children}
-        {links.demo && (
-          <Link href={links.demo} target="_blank">
-            <Button>Demo</Button>
-          </Link>
-        )}
-        {links.code && (
-          <Link href={links.code} target="_blank">
-            <Button variant="secondary">Código</Button>
-          </Link>
-        )}
-        <div className="flex flex-wrap mt-4 justify-center items-center gap-2">
-          {badges.map((badge, i) => (
-            <p
-              key={i}
-              className="px-3 py-1 last:mr-0 border border-primary-color border-solid rounded-full text-sm"
-            >
-              {badge}
-            </p>
-          ))}
+        <div>
+          <div className="flex flex-col items-center md:items-start lg:items-center gap-y-2 gap-x-4 lg:flex-row">
+            <h3 className="font-bold text-2xl text-white">{title}</h3>
+
+            <div className="flex flex-wrap justify-center items-center md:justify-start gap-2">
+              {badges.map((badge, i) => (
+                <p
+                  key={i}
+                  className="px-2 py-1 last:mr-0 border border-primary-color border-solid rounded-full text-xs"
+                >
+                  {badge}
+                </p>
+              ))}
+            </div>
+          </div>
+
+          <p className="mt-1 mb-6">{description}</p>
+
+          <div className="flex justify-center md:justify-start items-center gap-4">
+            {links.demo && (
+              <Link href={links.demo} target="_blank">
+                <Button>Demo</Button>
+              </Link>
+            )}
+            {links.code && (
+              <Link href={links.code} target="_blank">
+                <Button variant="secondary">Código</Button>
+              </Link>
+            )}
+          </div>
         </div>
-      </Card>
+      </div>
     </div>
   );
 };
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -6,16 +6,6 @@ import { ProjectCard } from "./ProjectCard";
 export const Projects: React.FC = () => {
   const projects = [
     {
-      title: "DevConnect",
-      description: "Plataforma de rede social para devs poderem se conectar",
-      imgSrc: devconnect,
-      links: {
-        demo: "https://devconnect.mbrunos.dev/",
-        code: "https://github.com/MBrunoS/devconnect",
-      },
-      badges: ["Next.js", "Typescript", "Prisma", "Markdoc"],
-    },
-    {
       title: "RepoWars",
       description: "Jogo de batalha de repositórios do GitHub",
       imgSrc: repowars,
@@ -25,20 +15,29 @@ export const Projects: React.FC = () => {
       },
       badges: ["React", "Typescript", "Vite", "React Icons"],
     },
+    {
+      title: "DevConnect",
+      description: "Plataforma de rede social para devs poderem se conectar",
+      imgSrc: devconnect,
+      links: {
+        demo: "https://devconnect.mbrunos.dev/",
+        code: "https://github.com/MBrunoS/devconnect",
+      },
+      badges: ["Next.js", "Typescript", "Prisma", "Markdoc"],
+    },
   ];
 
   return (
-    <div className="flex flex-col md:flex-row gap-4 items-center justify-around mt-8">
+    <div className="flex flex-col gap-4 items-center justify-around mt-16">
       {projects.map((project, i) => (
         <ProjectCard
           title={project.title}
           imgSrc={project.imgSrc}
           links={project.links}
           badges={project.badges}
+          description={project.description}
           key={i}
-        >
-          <p>{project.description}</p>
-        </ProjectCard>
+        />
       ))}
     </div>
   );
